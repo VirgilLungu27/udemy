e753af7532931afe704ae99e3f0bc333c001fa6a
Virgil <lunguvirgil3000@gmail.com>
2022-06-17 18:10:58 +0300

    Add event listener for the next button

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circle = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+next.addEventListener('click', () => {
+    currentActive++;
+});
