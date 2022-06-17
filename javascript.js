b1490ad28291e055e4e315ec226dd9347ef424df
Virgil <lunguvirgil3000@gmail.com>
2022-06-17 18:39:08 +0300

    Change disabled behaviour for prev and next to work correctly

--- javascript.js
+++ javascript.js
@@ -34,4 +34,14 @@ function update() {
 
     const actives = document.querySelectorAll('.active');3
     progress.style.width = parseInt((actives.length - 1) / (circles.length - 1) * 100) + '%';
+
+    if(currentActive === 1) {
+        prev.disabled = true;
+    } else if(currentActive === circles.length) {
+        next.disabled = true;
+    }
+      else {
+        prev.disabled = false;
+        next.disabled = false;
+      }
 }
