48dbaa42169e8465481e73853f981bca353c85dd
Virgil <lunguvirgil3000@gmail.com>
2022-06-19 18:12:10 +0300

    Add scale function with Stack Overflow help

--- script.js
+++ script.js
@@ -12,4 +12,12 @@ function blurring() {
         clearInterval(int);
     }
     loadText.innerText = `${load}%`;
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
 }
+
+/* Parameter added for changing opacity, since it needs to be the other way around compared to the loading page */
+let scale = (number, inMin, inMax, outMin, outMax) => {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
