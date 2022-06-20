f832f811cd5d29f763987a019ffcd1715f653341
Virgil <lunguvirgil3000@gmail.com>
2022-06-20 21:13:51 +0300

    Link script file and create labels constant

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-const left = document.querySelector('.left')
-const right = document.querySelector('.right')
-const container = document.querySelector('.container')
+const labels = document.querySelectorAll('.form-control label')
 
-left.addEventListener('mouseenter', () => container.classList.add('hover-left'));
-
-left.addEventListener('mouseleave', () => container.classList.remove('hover-left'));
-
-right.addEventListener('mouseenter', () => container.classList.add('hover-right'));
-
-right.addEventListener('mouseleave', () => container.classList.remove('hover-right'));
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('');
+        .map((letter, idx) => `<span>${letter}</span>`);
+        .join('');
+});
