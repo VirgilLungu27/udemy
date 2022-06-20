1e55990d16126da2fb58780c080d3b5685f48bfa
Virgil <lunguvirgil3000@gmail.com>
2022-06-20 19:07:57 +0300

    Add event listeners for the two sides

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-const boxes = document.querySelectorAll('.box');
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const container = document.querySelector('.container')
 
-window.addEventListener('scroll', checkBoxes);
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'));
 
-checkBoxes();
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'));
 
-function checkBoxes() {
-    const triggerBottom = window.innerHeight / 5 * 4;
-    boxes.forEach(box => {
-        const boxTop = box.getBoundingClientRect().top
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'));
 
-        if(boxTop < triggerBottom) {
-            box.classList.add('show');
-        } else {
-            box.classList.remove('show');
-        }
-    })
-}
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'));
