0e770138a03774102d02a45eb0415d9fb7ca9a0c
Virgil <lunguvirgil3000@gmail.com>
2022-06-21 19:19:04 +0300

    Link JS file

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+const jokeElement = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
 
-sounds.forEach(sound => {
-    const btn = document.createElement('button');
-    btn.classList.add('btn');
+generateJoke();
 
-    btn.innerText = sound;
-
-    btn.addEventListener('click', () => {
-        document.getElementById(sound).play()
-    });
-    document.getElementById('buttons').appendChild(btn);
-});
-
-function stopSongs() {
-    sounds.forEach(sound => {
-        const song = document.getElementById(sound);
-        
-        song.pause();
-        song.currentTime = 0;
-    })
-};
+function generateJoke() {
+    
+}
