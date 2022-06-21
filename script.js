d747a9db9d8d1066fc45f0e73b17db6ae3d70fc4
Virgil <lunguvirgil3000@gmail.com>
2022-06-21 19:22:10 +0300

    Create fetch request in JS file

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ const jokeBtn = document.getElementById('jokeBtn');
 generateJoke();
 
 function generateJoke() {
-    
+    const config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
+    fetch('https://icanhazdadjoke.com', config)   
 }
