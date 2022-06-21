e6c5f6e863e84405fd85693f20c2dbf72dd0f4d0
Virgil <lunguvirgil3000@gmail.com>
2022-06-21 19:37:14 +0300

    Make final changes. This does not work so will not be uploaded.

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const jokeElement = document.getElementById('joke');
 const jokeBtn = document.getElementById('jokeBtn');
 
+jokeBtn.addEventListener('click', generateJoke);
+
 generateJoke();
 
 function generateJoke() {
     const config = {
         headers: {
-            'Accept': 'application/json'
+            'Accept': 'application/json',
         }
     }
-    fetch('https://api.chucknorris.io/jokes/random', config).then(res => res.json()).then(data => console.log(data));
-}
+    fetch('https://api.chucknorris.io/jokes/random', config).then((res) => res.json()).then((data) => {
+        jokeElement.innerHTML = data.joke;
+    })
+};
