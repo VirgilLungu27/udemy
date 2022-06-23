b166bbe59412d61ddd7247412833b388770d599b
Virgil <lunguvirgil3000@gmail.com>
2022-06-23 17:26:26 +0300

    Make final changes to UpdateCounter in order to increment by 300 with every milisecond

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ counters.forEach(counter => {
     counter.innerText = '0';
 
     const updateCounter = () => {
-        const target = counter.getAttribute('data-target');
+        const target = +counter.getAttribute('data-target');
+        const c = +counter.innerText;
+
+        const increment = target / 300;
+
+        if(c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`;
+            setTimeout(updateCounter, 1);
+        } else {
+            counter.innerText = target;
+        }
     }
 
     updateCounter();
