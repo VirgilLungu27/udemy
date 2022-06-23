09174bb930ac61ed54d24bc221440a0dd479e509
Virgil <lunguvirgil3000@gmail.com>
2022-06-23 18:48:02 +0300

    Create highlightCups function

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ const remained = document.getElementById('remained');
 smallCups.forEach((cup, idx) => {
     cup.addEventListener('click', () => highlightCups(idx))
 })
+
+function highlightCups(idx) {
+    smallCups.forEach((cup, idx2) => {
+       if(idx2 <= idx) {
+          cup.classList.add('full');
+       } else {
+          cup.classList.remove('full');
+       }
+    })
+}
