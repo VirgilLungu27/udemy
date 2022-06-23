696e676df83afc3e9e5811ab8f422a06a9957935
Virgil <lunguvirgil3000@gmail.com>
2022-06-23 17:20:57 +0300

    Update function with updateCounter

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ const counters = document.querySelectorAll('.counter');
 
 counters.forEach(counter => {
     counter.innerText = '0';
+
+    const updateCounter = () => {
+        const target = counter.getAttribute('data-target');
+    }
+
+    updateCounter();
 })
