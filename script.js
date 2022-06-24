6593ba61862b6ddae4bdf4ac47d65fe8fab417da
Virgil <lunguvirgil3000@gmail.com>
2022-06-24 19:28:25 +0300

    Create function getMovies and use the API URL to log it into the console for now

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c5375c58e9cde67f839f2b85a5fa76c1&page=1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_URL = 'https://api.themoviedb.org/3/discover/movie?api_key=c5375c58e9cde67f839f2b85a5fa76c1&query="';
+
+// get initial movies
+getMovies(API_URL);
+
+async function getMovies(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    console.log(data.results);
+}
