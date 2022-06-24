12b7ec02b9159fb46cff9da3ba1ca94ef2233347
Virgil <lunguvirgil3000@gmail.com>
2022-06-24 19:46:11 +0300

    Fix search function by fixing the SEARCH URL

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c5375c58e9cde67f839f2b85a5fa76c1&page=1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-const SEARCH_API = 'https://api.themoviedb.org/3/discover/movie?api_key=c5375c58e9cde67f839f2b85a5fa76c1&query="';
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=c5375c58e9cde67f839f2b85a5fa76c1&query="';
 
 const form = document.getElementById('form');
 const search = document.getElementById('search');
