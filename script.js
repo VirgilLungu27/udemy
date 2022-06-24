62fb4aff4372d621a81c0668ea499dc62454b346
Virgil <lunguvirgil3000@gmail.com>
2022-06-24 19:26:00 +0300

    Add search URL to JS file

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c5375c58e9cde67f839f2b85a5fa76c1&page=1';
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280/'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_URL = 'https://api.themoviedb.org/3/discover/movie?api_key=c5375c58e9cde67f839f2b85a5fa76c1&query="';
