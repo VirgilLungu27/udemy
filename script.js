5282d304f88df93c483fb1027797c7c17e3dfff2
Virgil <lunguvirgil3000@gmail.com>
2022-06-25 16:51:48 +0300

    Declare days and months constants

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const secondElement = document.querySelector('.second');
 const timeElement = document.querySelector('.time');
 const dateElement = document.querySelector('.date');
 const toggleElement = document.querySelector('.toggle');
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
