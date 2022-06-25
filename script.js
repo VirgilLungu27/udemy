f0d8bb0af0876c5a41b008b4e9ac5529ac823454
Virgil <lunguvirgil3000@gmail.com>
2022-06-25 16:50:05 +0300

    Create JS file with six variables

--- script.js
+++ script.js
@@ -1,38 +1,6 @@
-const body = document.body;
-const slides = document.querySelectorAll('.slide');
-const leftBtn = document.getElementById('left');
-const rightBtn = document.getElementById('right');
-
-let activeSlide = 0;
-
-rightBtn.addEventListener('click', () => {
-    activeSlide++;
-    if(activeSlide > slides.length - 1) {
-        activeSlide = 0;
-    }
-    
-    setBgToBody();
-    setActiveSlide();
-})
-
-leftBtn.addEventListener('click', () => {
-    activeSlide--;
-    if(activeSlide < 0) {
-        activeSlide = slides.length-1;
-    }
-    
-    setBgToBody();
-    setActiveSlide();
-})
-
-setBgToBody()
-
-function setBgToBody() {
-    body.style.backgroundImage = slides[activeSlide].style.backgroundImage;    
-}
-
-function setActiveSlide() {
-    slides.forEach(slide => slide.classList.remove('active'));
-
-    slides[activeSlide].classList.add('active');
-}
+const hourElement = document.querySelector('.hour');
+const minuteElement = document.querySelector('.minute');
+const secondElement = document.querySelector('.second');
+const timeElement = document.querySelector('.time');
+const dateElement = document.querySelector('.date');
+const toggleElement = document.querySelector('.toggle');
