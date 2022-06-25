a51241683890eb8c4c5bf205094ce484a5947a87
Virgil <lunguvirgil3000@gmail.com>
2022-06-25 16:55:33 +0300

    Fix background color for the toggle css

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ const minuteElement = document.querySelector('.minute');
 const secondElement = document.querySelector('.second');
 const timeElement = document.querySelector('.time');
 const dateElement = document.querySelector('.date');
-const toggleElement = document.querySelector('.toggle');
+const toggle = document.querySelector('.toggle');
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html');
+
+    if(html.classList.contains('dark')) {
+        html.classList.remove('dark');
+        e.target.innerHTML = 'dark mode';
+    } else {
+        html.classList.add('dark');
+        e.target.innerHTML = 'light mode';
+    }
+});
