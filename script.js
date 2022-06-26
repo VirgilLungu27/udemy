b1ab17640adf2361071ce1f20558fecd8afdec65
Virgil <lunguvirgil3000@gmail.com>
2022-06-26 16:14:42 +0300

    Create event listener

--- script.js
+++ script.js
@@ -1,46 +1,21 @@
-const hourElement = document.querySelector('.hour');
-const minuteElement = document.querySelector('.minute');
-const secondElement = document.querySelector('.second');
-const timeElement = document.querySelector('.time');
-const dateElement = document.querySelector('.date');
-const toggle = document.querySelector('.toggle');
+const buttons = document.querySelectorAll('.ripple');
 
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        const x = e.clientX;
+        const y = e.clientY;
 
-toggle.addEventListener('click', (e) => {
-    const html = document.querySelector('html');
+        const buttonTop = e.target.offsetTop;
+        const buttonLeft = e.target.offsetLeft;
 
-    if(html.classList.contains('dark')) {
-        html.classList.remove('dark');
-        e.target.innerHTML = 'dark mode';
-    } else {
-        html.classList.add('dark');
-        e.target.innerHTML = 'light mode';
-    }
-});
+        const xInside = x - buttonLeft;
+        const yInside = y - buttonTop;
 
-function setTime() {
-    const time = new Date();
-    const month = time.getMonth();
-    const day = time.getDay();
-    const date = time.getDate();
-    const hours = time.getHours();
-    const hoursForClock = hours % 12;
-    const minutes = time.getMinutes();
-    const seconds = time.getSeconds();
+        const circle = document.createElement('span');
+        circle.classList.add('circle');
+        circle.style.top = yInside + 'px';
+        circle.style.left = xInside + 'px';
 
-    hourElement.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`;
-    minuteElement.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`;
-    secondElement.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`;
-
-    timeElement.innerHTML = `${hours}:${minutes < 10 ? `0${minutes}` :  minutes}`
-    dateElement.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
-}
-
-const scale = (num, in_min, in_max, out_min, out_max) => { 
-    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;}
-
-setTime();
-
-setInterval(setTime, 1000);
+        this.appendChild(circle);
+    })
+})
