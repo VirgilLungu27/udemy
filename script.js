dd6d0a0e26a932307f7cd3df27e92128ec7d97a2
Virgil <lunguvirgil3000@gmail.com>
2022-06-29 18:02:06 +0300

    Create slideLeft style top js

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ const slideRight = document.querySelector('.right-slide');
 const slideLeft = document.querySelector('.left-slide');
 const upButton = document.querySelector('.up-button');
 const downButton = document.querySelector('.down-button');
-const slidesLength = document.querySelectorAll('div').length;
+const slidesLength = slideRight.querySelectorAll('div').length;
+
+let activeSlideIndex = 0;
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
