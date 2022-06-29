f8f859a9787967af78f0f661628d816579bbca8a
Virgil <lunguvirgil3000@gmail.com>
2022-06-29 18:38:52 +0300

    create function createNotification in js file

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const button = document.getElementById('button');
 const toasts = document.getElementById('toasts');
 
-const messages = {
+const messages = [
     'message one',
     'message two',
     'message three',
     'message four',
+];
+
+button.addEventListener('click', () => createNotification());
+
+function createNotification() {
+    const notification = document.createElement('div');
+    notification.classList.add('toast');
+
+    notification.innerText = getRandomMessage();
+
+    toasts.appendChild(notification);
+}
+
+function getRandomMessage() {
+    return messages[Math.floor(Math.random() * messages.length)];
 }
