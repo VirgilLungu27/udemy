61b86c142e0d7d9b27a7725ca04c8bb7988ee853
Virgil <lunguvirgil3000@gmail.com>
2022-06-29 18:12:32 +0300

    add transform function to stylesheet

--- script.js
+++ script.js
@@ -18,7 +18,13 @@ const changeSlide = (direction) => {
         if(activeSlideIndex > slidesLength - 1) {
             activeSlideIndex = 0;
         }
+    } else if (direction === 'down') {
+        activeSlideIndex--;
+        if(activeSlideIndex < 0) {
+            activeSlideIndex = 0;
+        }
     }
 
     slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`;
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`;
 }
