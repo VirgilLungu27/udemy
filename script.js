2410f31628192e96d7d02575e45a8a6f7f516402
Virgil <lunguvirgil3000@gmail.com>
2022-06-30 18:05:22 +0300

    Add main div to js file to generate it by searching

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 const APIURL = 'https://api.github.com/users/';
 
+const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
 async function getUser(username) {
     try {
         const { data } = await axios(APIURL + username);
-        console.log(data);
+        createUserCard(data);
     } catch(error) {
         console.log(error);
     }
+};
+
+function createUserCard(user) {
+    const cardHTML = `<div class="card">
+    <div>
+        <img src="${user.avatar_url}" alt="${user.name}" class="avatar"> 
+    </div>
+    <div class="user-info">
+    <h2>${user.name}</h2>
+    <p>${user.bio}</p>
+    
+<ul>
+    <li>${user.followers} <strong>followers</strong></li>
+    <li>${user.following} <strong>following</strong></li>
+    <li>${user.public_repos} <strong>repos</strong></li>
+</ul>
+
+<div id="repos">
+</div>
+</div>`
+    main.innerHTML = cardHTML;
+
 }
 
 form.addEventListener('submit', (e) => {
@@ -22,4 +45,4 @@ form.addEventListener('submit', (e) => {
 
         search.value = '';
     }
-})
+});
