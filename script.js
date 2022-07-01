67da7f17c754880da505a42d9ec334153bfc65a9
Virgil <lunguvirgil3000@gmail.com>
2022-07-01 12:18:57 +0300

    create getRandomSymbol function

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ function getRandomUpper() {
 function getRandomNumber() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
+
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*()_+{}|=-<>/,.;'
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
