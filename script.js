557c890f28e32203948e11b22a4498b176e972cf
Virgil <lunguvirgil3000@gmail.com>
2022-07-01 10:53:48 +0300

    Add writeText function

--- script.js
+++ script.js
@@ -1,40 +1,11 @@
-const loveMe = document.querySelector('.loveMe');
-const times = document.querySelector('#times');
+const textElement = document.getElementById('text');
+const speedElement = document.getElementById('speed');
+const text = 'we love programming.';
+let idx = 1;
+let speed = 300 / speedElement.nodeValue;
 
-let clickTime = 0;
-let timesClicked = 0;
+writeText();
 
-loveMe.addEventListener('click', (e) => {
-    if(clickTime === 0) {
-        clickTime = new Date().getTime()        
-    } else {
-        if((new Date().getTime() - clickTime) < 600) {
-            createHeart(e);
-            clickTime = 0;
-        } else {
-            clickTime = new Date().getTime();
-        }
-    }
-});
-
-const createHeart = (e) => {
-    const heart = document.createElement('i');
-    heart.classList.add('fas');
-    heart.classList.add('fa-heart');
-
-    const x = e.clientX;
-    const y = e.clientY;
-
-    const leftOffset = e.target.offsetLeft;
-    const topOffset = e.target.offsetTop;
-
-    const xInside = x - leftOffset;
-    const yInside = y - topOffset;
-
-    heart.style.top = `${yInside}px`;
-    heart.style.left = `${xInside}px`;
-
-    loveMe.appendChild(heart);
-    times.innerHTML = ++timesClicked;
-    setTimeout(() => heart.remove, 2800)
+function writeText() {
+    textElement.innerText = text.slice(0, idx);
 }
