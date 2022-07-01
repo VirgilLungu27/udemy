006941ced5276843d1548f670daf5763eca56d89
Virgil <lunguvirgil3000@gmail.com>
2022-07-01 12:17:20 +0300

    create getRandomNumber function

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function getRandomLower() {
 function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random()* 26) + 65);
 }
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
