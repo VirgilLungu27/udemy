1f9d916116c783294322e144fbcc13e1aa817914
Virgil <lunguvirgil3000@gmail.com>
2022-07-01 12:27:14 +0300

    Create event listener

--- script.js
+++ script.js
@@ -14,6 +14,16 @@ const randomFunc = {
     symbol: getRandomSymbol
 }
 
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+})
+
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random()*26) + 97);
 }
@@ -30,4 +40,3 @@ function getRandomSymbol() {
     const symbols = '!@#$%^&*()_+{}|=-<>/,.;'
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
-
