500aa37d5a0062d33fabdebae177ef62c34a5d92
Virgil <lunguvirgil3000@gmail.com>
2022-07-02 14:45:32 +0300

    Create run function

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const leftButton = document.getElementById('left');
 const rightButton = document.getElementById('right');
 
 const img = document.querySelectorAll('#imgs img');
+
+let idx = 0;
+let interval = setInterval(run, 1500);
+
+function run() {
+    idx++;
+
+    changeImage();
+}
