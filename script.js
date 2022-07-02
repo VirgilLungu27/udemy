9cb40fa184e2b445c0052bc8a2a6b44f11a3b650
Virgil <lunguvirgil3000@gmail.com>
2022-07-02 12:45:48 +0300

    Create resetDOM function

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ const replay = document.querySelector('#replay');
 
 runAnimation() 
 
+function resetDOM() {
+    counter.classList.remove('hide');
+    finalMessage.classList.remove('show');
+
+    nums.forEach((num) => {
+        num.classList.value = '';
+    })
+    nums[0].classList.add('in');
+}
+
 function runAnimation() {
     nums.forEach((num, idx) => {
         const nextToLast = nums.length - 1;
@@ -15,7 +25,15 @@ function runAnimation() {
                 num.classList.add('out');
             } else if (e.animationName === 'goOut' && num.nextElementSibling) {
                 num.nextElementSibling.classList.add('in');
+            } else {
+                counter.classList.add('hide');
+                finalMessage.classList.add('show');
             }
         });
     })
 }
+
+replay.addEventListener('click', () => {
+    resetDOM();
+    runAnimation();
+})
