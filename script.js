b2151aa2be6b3c4d1c687e253ae18f07abaf14e1
Virgil <lunguvirgil3000@gmail.com>
2022-07-03 18:43:09 +0300

    create two event listeners inside the for loop

--- script.js
+++ script.js
@@ -6,5 +6,7 @@ for (let i = 0; i < SQUARES; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
 
+    square.addEventListener('mouseover', () => setColor(square));
+    square.addEventListener('mouseout', () => removeColor(square));
     container.appendChild(square);
 }
