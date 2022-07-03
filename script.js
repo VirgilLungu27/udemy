2714edaee527f9d4a11620c90d8848ab7230d452
Virgil <lunguvirgil3000@gmail.com>
2022-07-03 18:42:09 +0300

    Create for loop with square

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const container = document.getElementById('container');
 const colors = ['white', 'grey', 'rebeccapurple', 'steelblue', 'darkred', 'darkgreen', 'lightblue', 'orange', 'yellow'];
-const SQUARES = 500;
+const SQUARES = 499;
 
+for (let i = 0; i < SQUARES; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
 
+    container.appendChild(square);
+}
