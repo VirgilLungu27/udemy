56b0f4d61da7a1d3c8051f0782da657d238fe93b
Virgil <lunguvirgil3000@gmail.com>
2022-07-04 09:53:24 +0300

    Add classList for contents and items to foreach loop

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-const content = document.querySelectorAll('.content');
+const contents = document.querySelectorAll('.content');
 const listItems = document.querySelectorAll('nav ul li');
 
 listItems.forEach((item, idx) => {
     item.addEventListener('click', () => {
         hideAllContents();
         hideAllItems();
+
+        item.classList.add('active');
+        contents[idx].classList.add('show');
     })
 })
 
