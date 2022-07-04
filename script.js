3a6e31c3801eae83738eb422180581c1023bfb35
Virgil <lunguvirgil3000@gmail.com>
2022-07-04 12:13:11 +0300

    Create createBoxes function with two nested for loops

---
+++ script.js
@@ -0,0 +1,12 @@
+const boxesContainer = document.getElementById('boxes');
+const btn = document.getElementById('btn');
+
+function createBoxes() {
+    for(let i = 0; i < 4; i++) {
+        for(let j = 0; j < 4; j++) {
+            const box = document.createElement('div');
+        }
+    }
+}
+
+createBoxes();
