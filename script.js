bc69c46bb19f0dd678e04022fda900b5f10fbde2
Virgil <lunguvirgil3000@gmail.com>
2022-07-05 13:56:13 +0300

    Create filterData function

--- script.js
+++ script.js
@@ -29,3 +29,13 @@ async function getData() {
         result.appendChild(li);
     })
 }
+
+function filterData(searchTerm) {
+    listItems.forEach(item => {
+        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+            item.classList.remove('hide');
+        } else {
+            item.classList.add('hide');
+        }
+    })
+}
