732d0144d634d66a0c3c3b5cc0b98ccd9b6e7a3c
Virgil <lunguvirgil3000@gmail.com>
2022-07-06 06:39:25 +0300

    create event listener based on event bubbling

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const ratings = document.querySelectorAll('.rating');
 const sendBtn = document.querySelector('#send');
 const panel = document.querySelectorAll('#panel');
+
+panel.addEventListener('click', (e) => {
+    if (e.target.classList.contains('rating')) {
+
+    }
+})
