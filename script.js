e18fe30e58f72880a277bbc8dbb70bb48925f408
Virgil <lunguvirgil3000@gmail.com>
2022-07-06 10:13:28 +0300

    add script file iwth event listener

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
-const range = document.getElementById('range');
+const openBtn = document.querySelector('.open-btn');
+const closeBtn = document.querySelector('.close-btn');
+const nav = document.querySelectorAll('.nav');
 
-range.addEventListener('input', (e) => {
-    const value = +e.target.value;
-    const label = e.target.nextElementSibling;
-
-    const range_width = getComputedStyle(e.target).getPropertyValue('width');
-    const label_width = getComputedStyle(label).getPropertyValue('width');
-
-    const num_width = +range_width.substring(0, range_width.length - 2);
-    const num_label_width = +range_width.substring(0, label_width.length - 2);
-
-    const max = +e.target.max;
-    const min = +e.target.min;
-    
-    const left = value * (num_width / max) - num_label_width / 2 + scale(value, min, max, 10, -10);
-
-    label.style.left = `${left}px`
-
-    label.innerHTML = value;
+openBtn.addEventListener('click', () => {
+    nav.forEach(nav_el => nav_el.classList.add('visible'))
 })
-
-const scale = (num, in_min, in_max, out_min, out_max) => {
-    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
