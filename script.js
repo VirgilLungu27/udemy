3567c376c6b93c40f9d92ec4f9fb02e9132af24d
Virgil <lunguvirgil3000@gmail.com>
2022-07-06 08:50:45 +0300

    Add range_width constant

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ const range = document.getElementById('range');
 range.addEventListener('input', (e) => {
     const value = +e.target.value;
     const label = e.target.nextElementSibling;
+
+    const range_width = getComputedStyle(e.target).getPropertyValue('width');
     label.innerHTML = value;
 })
