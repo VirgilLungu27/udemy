159e5d8c40f1a153b33fb672c686b124e4d7dbba
Virgil <lunguvirgil3000@gmail.com>
2022-07-07 09:29:15 +0300

    Create quiz with three buttons

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-const openBtn = document.querySelector('.open-btn');
-const closeBtn = document.querySelector('.close-btn');
-const nav = document.querySelectorAll('.nav');
-
-openBtn.addEventListener('click', () => {
-    nav.forEach(nav_el => nav_el.classList.add('visible'))
-})
+const quizData = [
+    {
+        question: "Cand a avut loc rasturnarea lui Ceausescu?",
+        a: "1983",
+        b: "1989",
+        c: "1990",
+        d: "1992",
+        correct: "b",
+    },
+    {
+        question: "Unde a murit Hitler?",
+        a: "In buncarul lui din Berlin",
+        b: "Pe frontul din Stalingrad",
+        c: "In vizita la Paris",
+        d: "Intr-un cort de la Paschendale",
+        correct: "a",
+    },
+    {
+        question: "Cel mai bun jucator de fotbal din istoria Romaniei?",
+        a: "Ilie Dumitrescu",
+        b: "Adi Mutu",
+        c: "Razvan Rat",
+        d: "Gheorghe Hagi",
+        correct: "d",
+    }
+]
