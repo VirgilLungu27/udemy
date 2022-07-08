97392a69d8c6c61ad7294bcd7932d9031f0c80d2
Virgil <lunguvirgil3000@gmail.com>
2022-07-08 17:35:34 +0300

    Add getrandomNr function

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-const container = document.querySelector('.container')
-const unsplashURL = 'https://source.unsplash.com/random/'
+const container = document.querySelector('.container');
+const unsplashURL = 'https://source.unsplash.com/random/';
+const rows = 15
+
+for(let i = 0; i < rows * 4; i++) {
+    const img = document.createElement('img');
+    img.src = `${unsplashURL}`;
+}
+
+function getRandomNr() {
+    return Math.floor(Math.random() * 10) + 300;
+}
