e1dd42c4cbf1a97cd4bba12e2c6bd263b74ede31
Virgil <lunguvirgil3000@gmail.com>
2022-07-08 17:59:31 +0300

    create addTodo function and appendChild

--- script.js
+++ script.js
@@ -4,4 +4,27 @@ const todosUL = document.getElementById('todos');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    addTodo();
 });
+
+function addTodo(todo) {
+    let todoText = input.value;
+
+    if(todo) {
+        todoText = todo.text;
+    }
+
+    if(todoText) {
+        const todoEl = document.createElement('li');
+        if(todo && todo.completed) {
+            todoEl.classList.add('completed');
+        }
+
+        todoEl.innerText = todoText;
+
+        todosUL.appendChild(todoEl);
+
+        input.value = '';
+    }
+}
